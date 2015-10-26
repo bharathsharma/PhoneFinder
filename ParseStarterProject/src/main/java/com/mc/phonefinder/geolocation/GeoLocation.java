c572c4e0c56879cd58f27119c27e453e9a28b341
Rakesh627 <subramanian_rakesh@yahoo.co.in>
2015-10-26 01:25:26 -0700

    Modified GPS to update location

--- ParseStarterProject/src/main/java/com/mc/phonefinder/geolocation/GeoLocation.java
+++ ParseStarterProject/src/main/java/com/mc/phonefinder/geolocation/GeoLocation.java
@@ -115,34 +115,12 @@ public class GeoLocation extends ActionBarActivity implements LocationListener{
 
         return location;
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_geo_location, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onLocationChanged(Location location) {
 
     }
 
-    @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
