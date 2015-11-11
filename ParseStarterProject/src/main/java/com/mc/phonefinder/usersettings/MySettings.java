cbe3fe9804d040b1649f97832c0901fd89d3abe6
Rakesh627 <subramanian_rakesh@yahoo.co.in>
2015-11-10 18:08:54 -0700

    Added code to save user specific settings data in local and also in the server.

--- ParseStarterProject/src/main/java/com/mc/phonefinder/usersettings/MySettings.java
+++ ParseStarterProject/src/main/java/com/mc/phonefinder/usersettings/MySettings.java
@@ -1,6 +1,8 @@
 package com.mc.phonefinder.usersettings;
 
+import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,20 +13,98 @@ import android.widget.CheckBox;
 
 import com.mc.phonefinder.R;
 import com.mc.phonefinder.login.DispatchActivity;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import android.content.SharedPreferences;
+
+import java.util.List;
+
 
 public class MySettings extends ActionBarActivity {
 
+    public void savePrefs(String key, boolean value)
+    {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putBoolean(key,value);
+        edit.commit();
+    }
+
+    public boolean loadPrefs(String key)
+    {
+    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+    boolean cbVal =  sp.getBoolean(key, false);
+        return cbVal;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_settings);
-        
+        CheckBox checkbox1,checkbox2,checkbox3,checkbox4;
+        ParseUser user = ParseUser.getCurrentUser();
+        checkbox1 = (CheckBox) findViewById(R.id.alertCamera);
+        checkbox2= (CheckBox) findViewById(R.id.alertFall);
+        checkbox3 = (CheckBox) findViewById(R.id.alertLocation);
+        checkbox4 = (CheckBox) findViewById(R.id.alertUsers);
 
+        checkbox1.setChecked(loadPrefs(user.getObjectId()+String.valueOf(R.id.alertCamera)));
+        checkbox2.setChecked(loadPrefs(user.getObjectId()+String.valueOf(R.id.alertFall)));
+        checkbox3.setChecked(loadPrefs(user.getObjectId()+String.valueOf(R.id.alertLocation)));
+        checkbox4.setChecked(loadPrefs(user.getObjectId()+String.valueOf(R.id.alertUsers)));
 
         ((Button) findViewById(R.id.saveSettings)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            }});
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Settings");
+                query.whereEqualTo("userObjectId", ParseUser.getCurrentUser().getObjectId().toString());
+
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> scoreList, ParseException e) {
+                        //get current user
+                        ParseUser user = ParseUser.getCurrentUser();
+                        CheckBox checkbox1, checkbox2, checkbox3, checkbox4;
+                        checkbox1 = (CheckBox) findViewById(R.id.alertCamera);
+                        checkbox2 = (CheckBox) findViewById(R.id.alertFall);
+                        checkbox3 = (CheckBox) findViewById(R.id.alertLocation);
+                        checkbox4 = (CheckBox) findViewById(R.id.alertUsers);
+
+                        savePrefs(user.getObjectId()+String.valueOf(R.id.alertCamera), checkbox1.isChecked());
+                        savePrefs(user.getObjectId()+String.valueOf(R.id.alertFall), checkbox2.isChecked());
+                        savePrefs(user.getObjectId()+String.valueOf(R.id.alertLocation), checkbox3.isChecked());
+                        savePrefs(user.getObjectId()+String.valueOf(R.id.alertUsers), checkbox4.isChecked());
+
+                        if (e == null) {
+                            if (scoreList.size() > 0) {
+                                //store the location of the user with the objectId of the user
+                                scoreList.get(0).put("camera", checkbox1.isChecked());
+                                scoreList.get(0).put("userObjectId", user.getObjectId());
+                                scoreList.get(0).put("fall", checkbox2.isChecked());
+                                scoreList.get(0).put("location", checkbox3.isChecked());
+                                scoreList.get(0).put("otherUser", checkbox4.isChecked());
+
+                                scoreList.get(0).saveInBackground();
+                            } else {
+
+                                ParseObject locationObject = new ParseObject("Settings");
+                                locationObject.put("camera", checkbox1.isChecked());
+                                locationObject.put("fall", checkbox2.isChecked());
+                                locationObject.put("location", checkbox3.isChecked());
+                                locationObject.put("otherUser", checkbox4.isChecked());
+                                locationObject.put("userObjectId", user.getObjectId());
+                                locationObject.saveInBackground();
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+
+
+            }
+        });
 
                 // Starts an intent of the log in activity
 
